93827ccd95e873facd50a4cc759eb42c99740671
Yuriy Golota <ypgolota@pm.me>
2022-06-06 14:39:42 -0400

    base html done, with placeholder css

--- main.js
+++ main.js
@@ -1,38 +1,35 @@
 // Global Variables
 const body = document.getElementById('body');
 
-let searchTerm = 'illiterate';
+let searchTerm = 'illiterate+light';
 
 let parameterKeyValue = `media=music&term=${searchTerm}`;
 let fetchURL = `https://itunes.apple.com/search?${parameterKeyValue}`;
 
-// fetch(url, {
-//     method: 'GET',
-//     headers: { 'Content-Type': 'application/json' },
-//   })
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(`API fetch was successful. Data: ${data}`);
-//       createProfile(data);
-//     });
+console.log(`fetchURL: ${fetchURL}`);
 
+// API call to itunes
 fetch(fetchURL, {
     "method": "GET",
     "headers": {
         "content-type": "application/json"
     }
 })
-.then(response => {
-    console.log(response);
-    makePlayer(response);
+.then(function (response) {
+    console.log(`response: ${response}`);
+    return response.json();
+})
+.then(function (data) {
+    console.log(`API fetch was successful. Data: ${data}`);
+    main(data);
 })
 .catch(err => {
     console.error(err);
 });
 
-function makePlayer(response) {
-    let xyz = response.results[0].artistName
+// Main function, called in fetch()
+function main(data) {
+    console.log(`function main() called successfully.`);
+    let xyz = data.results[0].artistName;
     console.log(`xyz: ${xyz}`);
 }
