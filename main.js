b179074076f755ea539cd1db8112fe4be29f53a0
Yuriy Golota <ypgolota@pm.me>
2022-06-08 15:58:10 -0400

    small style tweaks

--- main.js
+++ main.js
@@ -2,61 +2,55 @@ listenForQuery()
 
 // Event listeners
 function listenForQuery(){
-
     const body_element = document.getElementById('body_element')
     const audio_element = document.getElementById('audio_element')
 
     search_bar.addEventListener('submit', (e) => {
         e.preventDefault()
         fetchItunesData()
-    })
-        
+    })  
     search_button.addEventListener('click', (e) => {
         e.preventDefault()
         fetchItunesData()
     })
-
     results_element.addEventListener('click', (e) => {
         audio_element.src = e.target.nextElementSibling.innerText
         audio_element.play()
     })
-
 }
 
 // API call to itunes
 function fetchItunesData(){
-
     clearPreviousResults()
     let fetchURL = createFetchURL()
 
     fetch(fetchURL, {
-        "method": "GET",
-        "headers": {
-            "content-type": "application/json"
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
         }
     })
     .then(function (response) {
-        console.log(response);
-        return response.json();
+        console.log(response)
+        return response.json()
     })
     .then(function (data) {
-        console.log(`API fetch was successful.`);
-        createResults(data);
+        console.log(data)
+        console.log('API fetch was successful.')
+        createResults(data)
     })
     .catch(err => {
-        console.error(err);
+        console.error(err)
     })
-
 }
 
 // Creates result elements & fills them with fetch data
 function createResults(data){
-
     let numResultsToDisplay = 45
 
     for (let i = 0; i < numResultsToDisplay; i++){
         
-        // Result element
+        // Result element (Parent for everything below)
         let result = document.createElement('div')
         result.classList.add('result_box')
         results_element.appendChild(result)
@@ -97,6 +91,7 @@ function createResults(data){
         audioSource3.innerText = data.results[i].previewUrl
         result.appendChild(audioSource3)
     }
+
     console.log('Finished loading search results.')
 }
 
@@ -112,7 +107,7 @@ function createFetchURL(){
     const itunesApiURL = 'https://itunes.apple.com/search?'
     const parameterKeyValue = 'media=music&term='
     const searchTerm = input.value
-    let fetchURL = itunesApiURL + parameterKeyValue + searchTerm
+    const fetchURL = itunesApiURL + parameterKeyValue + searchTerm
     
     console.log(`fetchURL: ${fetchURL}`)
 
